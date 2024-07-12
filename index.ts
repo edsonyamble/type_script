e79fe0b43905f5001f0070864621cf913b5ace90
Edson Yamble <edsonyamble2@gmail.com>
2024-07-12 10:54:27 +0700

    add variable

---
+++ index.ts
@@ -0,0 +1,15 @@
+// типы перемены
+// number
+// string
+// boolean
+// null
+// undefined
+// symbol
+// void
+const TheNumber: number = 10
+const TheString: string = 'Edson'
+
+const firstFunction = (): void => {
+	console.log(10)
+}
+//void travail avec les fonction 
