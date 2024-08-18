47a226a3ff011edf05de3458aa9cb417b445772b
Edson Yamble <edsonyamble2@gmail.com>
2024-08-18 16:09:12 +0700

    object

--- index.ts
+++ index.ts
@@ -1,72 +1,63 @@
-//example function
-//age can be in the function number or string
-const createPassword = (name: string, age: number | string) => {
-	;`My name is ${name} and I am ${age} years old`
+//object
+//типизация
+let obj: { name: string; city: string; popularty: string } = {
+	name: 'edson',
+	city: 'newyork',
+	popularty: '1000000000',
 }
-createPassword('Edson', 31)
-
-//default parameter
-const createPassword = (name: string = 'Edson', age: number | string = 31) => {
-	;`My name is ${name} and I am ${age} years old`
-}
-createPassword()
-
-//optional parameter
-const createSecondPassword = (name: string, age?: number) => {
-	;`My name is ${name} and I am ${age} years old`
-}
-createSecondPassword('Edson') //age is optional тоже не обязательно
-//rest type
-const createThirdPassword = (name: string, ...rest: number[]) => {
-	;`My name is ${name} and I am ${rest} years old`
-}
-createThirdPassword('Edson', 31, 32, 33) //my name is Edson and I am 31,32,33 years old
-//rest type
-const createfourPassword = (name: string, ...rest: Array<string | number>) => {
-	;`My name is ${name} and I am ${rest} years old`
-}
-createfourPassword('Edson', '31', 32, 'five') //my name is Edson and I am 31,32,five years old
-
-//return type string number or object
-const createdPassword = (name: string, age: number | string): string | number | object => {
-	return `My name is ${name} and I am ${age} years old`
-}
-createPassword('Edson', 31)
-
-//return string
-const creatednumPassword = (name: string, age: number | string): string => {
-	return `${name} and I am ${age} years old`
-}
-createPassword('Edson', 31)
-//return number
-const creatednumoPassword = (name: number, age: number): number => age+name
-
-//return object
-const createdobjectPassword = (name: string, age: number): object => {
-	return {
-		name:name,
-		age:age
-	}
-}
-//ничего не возвращать
-const creatednothingPassword = (name: string, age: number): void => {
-	
-}//это void
-
-//ошибка возращаемого типа
-const msg = "hello"
-const error = (msg: string): never => {
-	throw new Error(msg)
-}
-//функция выполняеться постоянно
-const infinite = (): never => {
-	while(true) {}
+console.log(obj)
+//obj типизация
+let obje: any = {
+	name: 'edson',
+	city: 'newyork',
+	popularty: '1000000000',
 }
 
-//fuunction variable 
-let functio: (name: string) => void;
+console.log(obje)
 
-function oldFunctio(name: string): void {
-	console.log(`hello ${name}`)
+//2 обект который одинокого типа
+let obje2: { name: string; city: string; popularty: string } = {
+	name: 'edson',
+	city: 'newyork',
+	popularty: '1000000000',
 }
-functio = oldFunctio 
+console.log(obje2)
+
+let obje3: { name: string; city: string; popularty: string } = {
+	name: 'edson',
+	city: 'newyork',
+	popularty: '1000000000',
+}
+console.log(obje3)
+//обект одинокого тип  dry  dont repeat yourself
+type user = { name: string; city: string; popularty: string }
+let obje4: user = {
+	name: 'edson',
+	city: 'newyork',
+	popularty: '1000000000',
+}
+console.log(obje4)
+
+let obje5: user = {
+	name: 'edson',
+	city: 'newyork',
+	popularty: '1000000000',
+}
+console.log(obje5)
+//2 обект одинокого типа примерно
+type usero = { name: string; city: string; popularty: string ;age?:number;color?: string}
+let obje6: usero = {
+	name: 'edson',
+	city: 'newyork',
+	popularty: '1000000000',
+	age: 31,
+}
+console.log(obje6)
+
+let obje7: usero = {
+	name: 'edson',
+	city: 'newyork',
+	popularty: '1000000000',
+	color: 'blue',
+}
+console.log(obje7)
