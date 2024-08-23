b767e23a84e01d8fd81f488efbab29296b7d41ff
Edson Yamble <edsonyamble2@gmail.com>
2024-08-23 12:19:46 +0700

    utility

--- index.ts
+++ index.ts
@@ -1,90 +1,93 @@
-//decorators in typescript это возможно тайпскрипт по добовление анотации для обявленя класс и функции
-//decorator это класс  который можно применять к любому обьекту и модифицировать его
 
-class User {
-	constructor(public name: string, public age: number) {}
-	getPass() {
-		return `user ${this.name} with age ${this.age}`
-	}
-}
-//base decorators  parametre constructor
-//4 тип decorator
-//тип класс 
-const logClass = (constructor: Function,) => {
-	console.log(constructor)
-}
-//apply decorator for class
-@logClass
-class User1 {
-	constructor(public name: string, public age: number) {}
-	getPass() {
-		return `user ${this.name} with age ${this.age}`
-	}
-}
-//тип свойство 
-const logProperty = (target: Object, propertyKey: string | symbol) => {
-	console.log(target)
-	console.log(propertyKey)
-}
-//apply decorator for property
+//utility их 16 не все будем смотреть 
+//1 readonly только для чичение 
+// interface User {
+// 	name: string
+// }
 
-class User2 {
- @logProperty
- secret:number 
-	constructor(public name: string, public age: number,secret:number) {this.secret=secret}
-	getPass() {
-		return `user ${this.name} with age ${this.age}`
-	}
-}
-//тип метод
-const logMethod = (target: Object, propertyKey: string, descriptor: PropertyDescriptor) => {
-	// console.log(target)
-	console.log(propertyKey)
-	// console.log(descriptor)
-}
-//apply decorator for method
+// const user:Readonly<User> = {
+// 	name: 'Yauhen'
+// }
+// user.name = 'Max'//ошибкуа будет потому что только для читение 
+//2 Required<T> все поля будут обязательными
+// interface User {
+// 	name: string
+// 	age: number
+// }
 
-class User3 {
-	constructor(public name: string, public age: number) {}
-	@logMethod
-	public getPass(): string {
-		return `user ${this.name} with age ${this.age}`
-	}
-}
-//getter and setter
-const logAccessor = (target: Object, propertyKey: string, descriptor: PropertyDescriptor) => {
-	// console.log(target)
-	console.log(propertyKey)
-	// console.log(descriptor)
-}
+// const user:Required<User> = {
+// 	name: 'Yauhen',
+// }
+//ошибкуа не будет потому что все поля обязательны
+//3 Partial<T> все поля будут опциональными то есть не обязателным 
+// interface User {
+// 	name: string
+// 	age: number
+// }
 
-class User4 {
-	constructor(public name: string, public age: number) {}
-	@logAccessor
-	set getPass(age: number) {
-		this.age = age
-	}
-}
-//factory decorator 
+// const user:Partial<User> = {
+// 	name: 'Yauhen',
+// }
+// user.name = 'Max'
+// //4 Records<T> создание словарей
+// interface PageInfo {
+// 	title: string
+// }
 
-const logFactory = (value:any) => {//factory
-return function (target: any) {//decorator
-	console.log(target)//decorator logic
-}
-}
-//apply decorator for factory
-const enumerable = (value: boolean) => {
-	return (target: any, propertyKey: string | symbol, descriptor: PropertyDescriptor) => {
-	descriptor.enumerable = value
-	}
-	
-}
+// type Page = 'home' | 'about' | 'contact'
 
-class User5 {
-	constructor(public name: string, public age: number) {}
-	@enumerable(false)//apply decorator for factory 
-	public getPass(): string {
-		return `user ${this.name} with age ${this.age}`
-	}
+// const x:Record<Page, PageInfo> = {
+// 	about: {title: 'about'},
+// 	home: {title: 'home'},
+// 	contact: {title: 'contact'},
+// }
+//5 Pick<T, K>
+// interface PageInfo {
+// 	title: string
+// 	description: string
+// 	completed: boolean
+// }
+
+// type Page =  Pick<PageInfo, 'title' | 'completed'>
+
+// const pageinfo:Page = {
+// 	title: 'about',
+// 	completed: false
+// }
+//6 Omit<T, K> это без какие то данных 
+// interface PageInfo {
+// 	title: string
+// 	description: string
+// 	completed: boolean
+// }
+
+// type Page = Omit<PageInfo, 'title' | 'completed'>
+
+// const pageinfo: Page = {
+// 	description: 'about'
+// }
+//7 exclude<T, U> удаляет дублированные данные
+// type PageInfo = Exclude< "title" | 'completed' | 'description', 'description'>//title | completed
+// //8 Extract<T, U> получаем дублированные данные
+// type Page = Extract< "title" | 'completed' | 'description', 'title' | 'completed'>//title | completed
+// //9 NonNullable<T> получаем не null и не undefined
+// type Page2 = NonNullable< "title" | 'completed' | 'description' | null | undefined>//title | completed | description
+//10 Return type  получение тип  function 
+// declare function f(): {
+// 	a: number;
+// 	b: string;
+// };
+// type T0 = ReturnType<typeof f>;//{a: number, b: string}
+// type T1 = ReturnType<() => string>;//string
+// type T2 = ReturnType<any>;//any
+// type T3 = ReturnType<never>;//never
+//11 InstanceType<T> получение типа class 
+class c {
+	a: number
+	b: string
 }
+type T0 = InstanceType<typeof c>;//c
+type T1 = InstanceType<new () => c>;//c
+type T2 = InstanceType<any>;//any
+type T3 = InstanceType<never>;//never
 
